cc6465157c19c763f14a477c71d29b16da1ce489
alfanherya <alfanherya@gmail.com>
2022-11-20 19:12:54 +0700

    golangdocumentation - add new code

---
+++ golangdocumentation/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/rizalgowandy/go-swag-sample/docs/echosimple"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
+
+func main() {
+	e := echo.New()
+
+	// add middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	// add routes
+	e.GET("/", HealtCheck)
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+	// e.GET("/", func(c echo.Context) error {
+	// 	return c.String(http.StatusOK, "hello herya!")
+	// })
+	e.Logger.Fatal(e.Start(":3000"))
+}
+
+func HealtCheck(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"data": "server is up and running",
+	})
+}
